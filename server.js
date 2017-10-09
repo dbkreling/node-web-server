0dc75b7d04311d4f73f9a5b7f04a2e22a1f3d3e8
Daniel Kreling <dbkreling@gmail.com>
2017-10-09 17:51:42 -0300

    server: implement dynamic contents with hbs
    Introducing hbs module to load contents from templates
    
    Ref.: Lecture 43
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res ) => {
-    // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Daniel',
-        likes: [
-            'music',
-            'citites'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Home Page',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page.');
+    res.render('about.hbs', {
+        pageTitle: "About Page",
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad',(req, res) => {
