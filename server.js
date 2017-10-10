ad99988257ef6f18c927a489817aa21b4ef99d89
Daniel Kreling <dbkreling@gmail.com>
2017-10-10 07:16:19 -0300

    server.js: add maintenance function
    A maintenance module is added to server.js in order to put the website into maintenance.
    This is part of the exploration of middleware functions.
    
    Ref.: Lecture 45
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+// Uncomment the next function out in order to put the site under maintenance
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -33,6 +52,7 @@ app.get('/bad',(req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
